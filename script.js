dfcdade05d912bac88e29bf2a7aa84c80400bdd9
Alex Shalynin <vishes0@gmail.com>
2017-02-19 00:45:21 +0300

    changes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-$(document).ready(function () {
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
     
     var wow = new WOW(
   {
